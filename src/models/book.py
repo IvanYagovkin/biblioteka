031be65ff32224a710b84eb5ef47a51a188d41de
unknown <trio.tri.ttt@gmail.com>
2025-10-28 13:49:44 +0400

    test

--- src/models/book.py
+++ src/models/book.py
@@ -4,7 +4,14 @@ class Book:
         self.author = author
         self.year = year
         self.isbn = isbn
+        self.is_available = True
 
     def __str__(self) -> str: 
         return f"'{self.title}', автор - {self.author}, год публикации - {self.year}"
-
+    '''Бронирование книги'''
+    def borrow(self) -> str:
+        if self.is_available:
+            self.is_available = False
+            return f"Книга'{self.title}' выдана"
+        else:
+            return f"Выдача книги {self.title} невозможна!"
